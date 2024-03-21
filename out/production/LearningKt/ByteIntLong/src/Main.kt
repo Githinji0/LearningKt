855ee0253e59b529d6ee190cc8de12eaab6ff62c
WilliamGithinji <146653695+Githinji0@users.noreply.github.com>
2024-03-21 12:25:26 +0300

    Add files via upload

---
+++ out/production/LearningKt/ByteIntLong/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    var name: String = "william"
+    val age: Int = 20
+
+    val maxIntegerValue: Int = Int.MAX_VALUE //to determine the maximum value of integers that can be stored
+    val minIntegerValue: Int = Int.MIN_VALUE //and vice versa
+
+    println("the max integer value is $maxIntegerValue while the min is $minIntegerValue")
+
+    val myMaxByte = Byte.MAX_VALUE
+    val myMinByte = Byte.MIN_VALUE
+
+    println("my max value is $myMaxByte while min is $myMinByte")
+
+    val myShortMax = Short.MAX_VALUE
+    val myShotMin = Short.MIN_VALUE
+
+    println("my max short value is $myShortMax while min is $myShotMin")
+
+    val myLongMax = Long.MAX_VALUE
+    val myLongMin = Long.MIN_VALUE
+
+    println("my max long value is $myLongMax while min is $myLongMin")
+
+    val myNum = 28
+}
+
---
+++ out/production/LearningKt/Variables/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var nameUser: String = "William"
+
+    //var nameUser = "william"
+    println("hello $nameUser")
+    var age: Int = 20
+
+    println("you are $age years old")
+}
