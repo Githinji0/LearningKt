cab29b85f61ded67de54055bbc278855b416cc45
WilliamGithinji <146653695+Githinji0@users.noreply.github.com>
2024-03-28 01:27:37 +0300

    Add files via upload

--- ArrayAplication/src/Main.kt
+++ ArrayAplication/src/Main.kt
@@ -1,3 +1,27 @@
+
+
 fun main() {
-    
+    var max = findMax(arrayOf(1,2,3,4,50,6,7,8))
+    var min = findMin(arrayOf(2,2,5,6,7,8,9,0,90))
+    println(max)
+    println(min)
+}
+
+fun findMax(numbers: Array<Int>): Int {
+    var max = numbers[0]
+    for (number in numbers){
+        if (number > max){
+            max = number
+        }
+    }
+    return max
+}
+fun findMin(numbers: Array<Int>): Int {
+    var min = numbers[0]
+    for (number in numbers){
+        if (number < min){
+            min = number
+        }
+    }
+    return min
 }
