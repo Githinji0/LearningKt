bfc55595a682142594b2a15ae0e3ad2fc312817a
WilliamGithinji <146653695+Githinji0@users.noreply.github.com>
2024-04-03 21:17:39 +0300

    Add files via upload

---
+++ OOP/Maps/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+//    var things = mapOf<Int, String>(1 to "car", 2 to "bus",3 to "pot")
+    var things = mutableMapOf<Int, String>(1 to "car", 2 to "bus",3 to "pot")
+
+    println(things[1])
+}
